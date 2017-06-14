c49313bf1ea90fa77e2b39fde547fe9dd68bf48c
Christophe Galon <cgalon@free.fr>
2017-06-14 14:30:13 +0200

    feat(ping): ajoute un service de ping (et vire la securite pour Actuator)

---
+++ src/main/java/fr/mojope/hackathon/Ping.java
@@ -0,0 +1,16 @@
+package fr.mojope.hackathon;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by christophe on 14/06/17.
+ */
+@RestController
+public class Ping {
+
+    @RequestMapping("/ping")
+    public String ping() {
+        return "OK";
+    }
+}
