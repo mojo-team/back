81d7d580a7e7eae5953971010184e8df46ad29e2
Christiansen MEREL <christiansen.merel@gmail.com>
2017-06-14 15:17:38 +0200

    chore: add travis build

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -x
+
+sshpass -e scp target/*.jar vivatech@vivatech.pintade.org:/home/vivatech/
+sshpass -e ssh vivatech@vivatech.pintade.org sudo systemctl stop vivatech
+sshpass -e ssh vivatech@vivatech.pintade.org sudo systemctl start vivatech
