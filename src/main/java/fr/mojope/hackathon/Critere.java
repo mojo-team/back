51f5dd86ff34fa29d762bb05376d6158ad389dff
Christophe Galon <cgalon@free.fr>
2017-06-16 23:35:21 +0200

    feat(salles): ajoute la liste des criteres de recherche de salles

---
+++ src/main/java/fr/mojope/hackathon/Critere.java
@@ -0,0 +1,15 @@
+package fr.mojope.hackathon;
+
+/**
+ * Created by christophe on 16/06/17.
+ */
+public class Critere {
+
+    final public int code;
+    final public String libelle;
+
+    public Critere(int code, String libelle) {
+        this.code = code;
+        this.libelle = libelle;
+    }
+}
