c83a682241a41ae195c38bb1f20f202a46edd05e
BenPeyr <benjamin.peyresaubes@etudiant.mines-nantes.fr>
2017-06-17 10:47:01 +0200

    correction

--- src/main/java/fr/mojope/hackathon/chabot/facebook/FacebookController.java
+++ src/main/java/fr/mojope/hackathon/chabot/facebook/FacebookController.java
@@ -26,10 +26,11 @@ public class FacebookController {
 		if(firstTimeIncident) {
 			firstTimeIncident = false;
 			facebookSender.sendMessage(scriptingManager.getLastUserId(), String.format("Don't worry %s, Robert is here for you. First, I need to know what's wrong : you can tell me by picking one of the common reasons, or you can directly write your problem.", scriptingManager.getLastUserFirstName()));
-			return facebookSender.sendMessageQuickReplies(scriptingManager.getLastUserId(), "I can also see that you are currently in a meeting room. I'm adding it to the list.", "Actual room", "Precedent room", "Room reservation", "Other service", "Billing", "Sodexo card");
+			facebookSender.sendMessageQuickReplies(scriptingManager.getLastUserId(), "I can also see that you are currently in a meeting room. I'm adding it to the list.", "Actual room", "Precedent room", "Room reservation", "Other service", "Billing", "Sodexo card");
 		}else {
-			return facebookSender.sendMessageQuickReplies(scriptingManager.getLastUserId(), String.format("Don't worry %s, Robert is here for you. First, I need to know what's wrong : you can tell me by picking one of the common reasons, or you can directly write your problem.", scriptingManager.getLastUserFirstName()), "Precedent room", "Room reservation", "Other service", "Billing", "Sodexo card");
+			facebookSender.sendMessageQuickReplies(scriptingManager.getLastUserId(), String.format("Don't worry %s, Robert is here for you. First, I need to know what's wrong : you can tell me by picking one of the common reasons, or you can directly write your problem.", scriptingManager.getLastUserFirstName()), "Precedent room", "Room reservation", "Other service", "Billing", "Sodexo card");
 		}
+		return "";
 	}
 	
 	@RequestMapping("/pushavis")
