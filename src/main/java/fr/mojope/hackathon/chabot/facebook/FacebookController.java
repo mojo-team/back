693b722426319e7c61a30f5e50241cbf460d9020
BenPeyr <benjamin.peyresaubes@etudiant.mines-nantes.fr>
2017-06-17 10:02:02 +0200

    Correcting rest endpoint

--- src/main/java/fr/mojope/hackathon/chabot/facebook/FacebookController.java
+++ src/main/java/fr/mojope/hackathon/chabot/facebook/FacebookController.java
@@ -22,15 +22,15 @@ public class FacebookController {
 	private FacebookSender facebookSender;
 	
 	@RequestMapping("/pushincident")
-	public void requestIncident() {
+	public String requestIncident() {
 		if(firstTimeIncident) {
+			firstTimeIncident = false;
 			facebookSender.sendMessage(scriptingManager.getLastUserId(), String.format("Don't worry %s, Robert is here for you. First, I need to know what's wrong : you can tell me by picking one of the common reasons, or you can directly write your problem.", scriptingManager.getLastUserFirstName()));
 			facebookSender.sendMessageQuickReplies(scriptingManager.getLastUserId(), "I can also see that you are currently in a meeting room. I'm adding it to the list.", "Actual room", "Precedent room", "Room reservation", "Other service", "Billing", "Sodexo card");
-			firstTimeIncident = false;
 		}else {
 			facebookSender.sendMessageQuickReplies(scriptingManager.getLastUserId(), String.format("Don't worry %s, Robert is here for you. First, I need to know what's wrong : you can tell me by picking one of the common reasons, or you can directly write your problem.", scriptingManager.getLastUserFirstName()), "Precedent room", "Room reservation", "Other service", "Billing", "Sodexo card");
 		}
-		
+		return "";
 	}
 	
 	@RequestMapping("/pushavis")
