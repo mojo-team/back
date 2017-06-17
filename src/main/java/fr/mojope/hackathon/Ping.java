d71eec23780479a7dac4d99d498ad700f4e8bcbf
BenPeyr <benjamin.peyresaubes@etudiant.mines-nantes.fr>
2017-06-17 11:26:32 +0200

    correction

--- src/main/java/fr/mojope/hackathon/Ping.java
+++ src/main/java/fr/mojope/hackathon/Ping.java
@@ -11,7 +11,7 @@ public class Ping {
 
     @RequestMapping("/ping")
     public String ping() {
-        return "OK3";
+        return "OK4";
     }
 
     @RequestMapping("/")
--- src/main/java/fr/mojope/hackathon/chabot/facebook/FacebookController.java
+++ src/main/java/fr/mojope/hackathon/chabot/facebook/FacebookController.java
@@ -23,13 +23,13 @@ public class FacebookController {
 	
 	@RequestMapping("/pushincident")
 	public String requestIncident() {
-		if(firstTimeIncident) {
-			firstTimeIncident = false;
+//		if(firstTimeIncident) {
+//			firstTimeIncident = false;
 			facebookSender.sendMessage(scriptingManager.getLastUserId(), String.format("Don't worry %s, Robert is here for you. First, I need to know what's wrong : you can tell me by picking one of the common reasons, or you can directly write your problem.", scriptingManager.getLastUserFirstName()));
 			facebookSender.sendMessageQuickReplies(scriptingManager.getLastUserId(), "I can also see that you are currently in a meeting room. I'm adding it to the list.", "Actual room", "Precedent room", "Room reservation", "Other service", "Billing", "Sodexo card");
-		}else {
-			facebookSender.sendMessageQuickReplies(scriptingManager.getLastUserId(), String.format("Don't worry %s, Robert is here for you. First, I need to know what's wrong : you can tell me by picking one of the common reasons, or you can directly write your problem.", scriptingManager.getLastUserFirstName()), "Precedent room", "Room reservation", "Other service", "Billing", "Sodexo card");
-		}
+//		}else {
+//			facebookSender.sendMessageQuickReplies(scriptingManager.getLastUserId(), String.format("Don't worry %s, Robert is here for you. First, I need to know what's wrong : you can tell me by picking one of the common reasons, or you can directly write your problem.", scriptingManager.getLastUserFirstName()), "Precedent room", "Room reservation", "Other service", "Billing", "Sodexo card");
+//		}
 		return "";
 	}
 	
